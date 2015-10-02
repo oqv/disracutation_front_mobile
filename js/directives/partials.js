882019e0b8c921db7f87b8a23210e12c0e349aa2
Victor <victor.perdomo12@gmail.com>
2015-10-02 20:23:55 -0300

    Diretivas

--- js/directives/partials.js
+++ js/directives/partials.js
@@ -2,31 +2,38 @@
 app.directive('headermobi', function() {
   return {
     restrict: 'E',
-    scope: {
-      item: '='
-    },
     templateUrl: 'views/partials/header.html'
   };
-})
+});
 
 //footer
 app.directive('footermobi', function() {
   return {
     restrict: 'E',
-    scope: {
-      item: '='
-    },
     templateUrl: 'views/partials/footer.html'
   };
-})
+});
 
 //breadcrumb
 app.directive('breadcrumb', function() {
   return {
     restrict: 'E',
-    scope: {
-      item: '='
-    },
     templateUrl: 'views/partials/breadcrumb.html'
   };
-})
+});
+
+//filters e order by
+app.directive('filters', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'views/partials/filters.html'
+  };
+});
+
+//pagination
+app.directive('pagination', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'views/partials/pagination.html'
+  };
+});
