ec0639688b41eaee4ddbd6b086d8105b4d8a66ce
Victor Alencar <victor.alencar@oqvestir.com.br>
2015-10-06 16:05:20 -0300

    conf especifica prod

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ app.config(function(RestangularProvider, $locationProvider) {
 
    $locationProvider.html5Mode(true);
 
-   RestangularProvider.setBaseUrl('http://chunli-homologation.elasticbeanstalk.com/api/v1');
+   RestangularProvider.setBaseUrl('http://chunli-production.elasticbeanstalk.com/api/v1');
    RestangularProvider.setDefaultHttpFields({cache: true});
 
 });
